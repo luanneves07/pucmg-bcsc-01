626afa8e5380774310084dfdb72290b930e4aebe
luanneves07 <luannevessilva@gmail.com>
2021-12-18 15:31:08 -0300

    BLCK-0002 - Changes contract

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,13 +4,14 @@ contract Marketplace {
   string public name;
   uint public productCount=0;
   mapping(uint => Product) public products;
+  address payable ownerAdderss = 0x1606cE047B38d5b3d714Db5728E7827b9E0D5AF4;
 
 struct Product {
   uint id;
   string name;
   uint price;
   address payable owner;
-  bool purchased;
+  bool forSale;
 }
 
 event ProductCreated (
@@ -18,7 +19,7 @@ event ProductCreated (
   string name,
   uint price,
   address payable owner,
-  bool purchased
+  bool forSale
 );
 
 event ProductPurchased (
@@ -26,8 +27,17 @@ event ProductPurchased (
   string name,
   uint price,
   address payable owner,
-  bool purchased
+  bool forSale
 );
+
+event ProductUpdated (
+  uint id,
+  string name,
+  uint price,
+  address payable owner,
+  bool forSale
+);
+
   constructor() public {
     name = "Dapp University Marketplace";
   }
@@ -42,7 +52,31 @@ event ProductPurchased (
     //Create the product
     products[productCount] = Product(productCount, _name, _price, msg.sender, false);
     //Trigger an event
-    emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    emit ProductCreated(productCount, _name, _price, msg.sender, true);
+  }
+
+  function changePrice(uint _id, uint _price) public {
+    //Requiere a valid price
+    require(_price > 0, "Enter a valid price");
+    //Make sure the product has valid id
+    require(_id > 0 && _id <= productCount, "Enter valid id");
+    //Changes product price
+    Product memory _product = products[_id];
+    _product.price = _price;
+    products[_id] = _product;
+    //Trigger an event
+    emit ProductUpdated(_id, products[_id].name, products[_id].price, msg.sender, products[_id].forSale);
+  }
+
+  function changeForSalestate(uint _id, bool forSale) public {
+    //Make sure the product has valid id
+    require(_id > 0 && _id <= productCount, "Enter valid id");
+    //Changes product for sale flag
+    Product memory _product = products[_id];
+    _product.forSale = forSale;
+    products[_id] = _product;
+    //Trigger an event
+    emit ProductUpdated(_id, products[_id].name, products[_id].price, msg.sender, products[_id].forSale);
   }
 
   function purchaseProduct(uint _id) public payable {
@@ -54,19 +88,19 @@ event ProductPurchased (
     require(_product.id > 0 && _product.id <= productCount, "Enter valid id");
     //Require that there is enough Ether in the transaction
     require(msg.value >= _product.price,"Transfer the correct amount");
-    //Require that the product has not been purchased already
-    require(!_product.purchased, "Product has been purchased");
+    //Require that the product is for sale
+    require(_product.forSale, "Product not for sale");
     //Require that the buyer is not the seller
     require(msg.sender != _seller, "Buyer cannot be seller");
     //Transfer ownership to the buyer
     _product.owner = msg.sender;
-    //Mark as purchased
-    _product.purchased = true;
     //Update the product
     products[_id] = _product;
-    //Pay the seller by sending them Ether
-    address(_seller).transfer(msg.value);
+    //Sends 5% for platform owner
+    ownerAdderss.transfer((msg.value*5)/100);
+    //Pay the seller by sending them Ether (95% of price)
+    address(_seller).transfer((msg.value*95)/100);
     //Trigger an event
-    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, products[_id].forSale);
   }
 }
